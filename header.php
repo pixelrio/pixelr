1c6b0468eee01855568566a654c1314d125d83ac
Gary Bhanot <info@pixelr.io>
2024-01-27 22:34:27 -0500

    Pushed Google Analytics code

--- header.php
+++ header.php
@@ -28,6 +28,16 @@
   <!-- Font-Awesome Icons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y7624RQXT4"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-Y7624RQXT4');
+	</script>
+
 	<?php wp_head(); ?>
 	
 </head>
